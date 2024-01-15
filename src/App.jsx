b6405e5f5ee143c49849c6cc1271c9b6dd3103cc
Shafayet-tuhin <shafayettuhin00@gmail.com>
2024-01-15 21:09:03 +0600

    reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,80 @@
-import { useReducer } from 'react';
+import { useEffect, useReducer } from 'react';
 import './App.css';
-import { noteReducer } from './reducers/NoteReducer';
+
+const StudentsArray = JSON.parse(localStorage.getItem('key')) || [];
 
 const initState = {
-  noteTitle: "", // Fixed the typo here
-  notes: [],
+  noteTitle: "",
+  notes: StudentsArray,
   editMode: false,
   editableNote: null,
 };
 
+const noteReducer = (currentState, action) => {
+  switch (action.type) {
+    case 'Change_Title': {
+      return {
+        ...currentState,
+        noteTitle: action.payload,
+      };
+    }
 
+    case 'Create_Note': {
+      const newNote = {
+        id: Date.now(),
+        title: currentState.noteTitle,
+      };
+      return {
+        ...currentState,
+        notes: [...currentState.notes, newNote],
+        noteTitle: "",
+      };
+    }
+    
+    case 'Edit_Note': {
+      const tobeUpdated = currentState.notes.find(el => el.id === action.payload);
+
+      return {
+        ...currentState,
+        editMode: true,
+        editableNote: tobeUpdated,
+        noteTitle: tobeUpdated.title,
+      };
+    }
+
+    case 'Update_Note': {
+      return {
+        ...currentState,
+        notes: currentState.notes.map(item => {
+          if (item.id === currentState.editableNote.id) {
+            item.title = currentState.noteTitle;
+          }
+          return item;
+        }),
+        editMode: false,
+        editableNote: null, 
+        noteTitle: "",
+      };
+    }
+    
+    case 'Remove_Note': {
+      return {
+        ...currentState,
+        notes: currentState.notes.filter(item => item.id !== action.payload),
+      };
+    }
+
+    default:
+      return currentState;
+  }
+};
 
 function App() {
   const [notestStates, dispatch] = useReducer(noteReducer, initState);
+
+  useEffect(() => {
+    localStorage.setItem('key', JSON.stringify(notestStates.notes));
+  }, [notestStates.notes]);
 
   return (
     <div>
