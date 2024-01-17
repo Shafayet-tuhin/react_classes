518eaa1986af4c4955656c36c3f146fee158d820
Shafayet-tuhin <shafayettuhin00@gmail.com>
2024-01-17 22:17:10 +0600

    reducer

--- src/components/Attendance.jsx
+++ src/components/Attendance.jsx
@@ -3,32 +3,8 @@ import { studentContext } from '../contexts/StudentProvider'
 
 
 const Attendance = () => {
-
-  const data = useContext(studentContext)
-
-
-    const changeStudent = (id) => {
-        const targetStudent = data.students.find(item => item.id === id)
-
-        const updatedStudents = data.students.map((item) => {
-            if (item.id == targetStudent.id) {
-
-                item.isPresent = !item.isPresent;
-
-                // if ( item.isPresent == false){
-                //   item.isPresent = true ;
-                // }
-                // else {
-                //   item.isPresent = false ;
-                // }
-
-            }
-            return item;
-        })
-        data.setStudents(updatedStudents);
-    }
-
-
+ 
+    const { studnetState, dispatch } = useContext(studentContext);
 
     return (
         <>
@@ -36,10 +12,10 @@ const Attendance = () => {
                 <h2>Present Students</h2><hr />
                 <ul>
                     {
-                        data.students.filter(item => item.isPresent === true).map(item => (
+                        studnetState.students.filter(item => item.isPresent === true).map(item => (
                             <li key={item.id}>
                                 <span>{item.name}</span>
-                                <button onClick={() => {changeStudent(item.id) }}>Remove</button>
+                                <button onClick={() => dispatch({type : 'Toggle' , value : item.id})}>Remove</button>
                             </li>
                         ))
                     }
@@ -51,10 +27,10 @@ const Attendance = () => {
                 <h2>Absent Students</h2><hr />
                 <ul>
                     {
-                        data.students.filter(item => item.isPresent === false).map(item => (
+                        studnetState.students.filter(item => item.isPresent === false).map(item => (
                             <li key={item.id}>
                                 <span>{item.name}</span>
-                                <button onClick={() => { changeStudent(item.id) }}>Remove</button>
+                                <button onClick={() => dispatch({type : 'Toggle' , value : item.id})}>Remove</button>
                             </li>
                         ))
                     }
--- src/components/StudentForm.jsx
+++ src/components/StudentForm.jsx
@@ -3,48 +3,16 @@ import { studentContext } from '../contexts/StudentProvider';
 
 const StudentForm = () => {
 
-   const {studentName , setStudentName , students , setStudents , editableStudent , setEditableStudent , editMode ,setEditMode } = useContext(studentContext)
-  const createStudentHandler = (e) => {
-    e.preventDefault() ; 
-
-    if( !studentName ) {
-      return alert("Please enter a name") ; 
-    }
-
-    const newStudent = {
-      id : Date.now(),
-      name : studentName , 
-      isPresent : undefined ,
-    }
-    setStudents([...students,newStudent]) ; 
-    setStudentName("") ;
-  } 
-
-  const updateHandler = (el) => {
-    el.preventDefault() ; 
-
-    if(!studentName ) {
-      return alert("Please enter a name") ; 
-    }
-   
-    const updateStudentsArray = students.map ( (item) => {
- 
-       if ( item.id === editableStudent.id) {
-        item.name = studentName ; 
-       }
-       return item ; 
-    })
- 
-    setStudents(updateStudentsArray) ;
-    setEditMode(false) ;
-    setEditableStudent(null) ;
-    setStudentName("") ;
-  }
-
+    const { studnetState, dispatch } = useContext(studentContext);
+  
+  
   return (
-          <form onSubmit={editMode ? updateHandler : createStudentHandler}>
-            <input type="text" value={studentName} onChange={(el) => setStudentName(el.target.value)} />
-            <button type="submit">{editMode ? "Update Student" : "Add Student"}</button>
+          <form onSubmit={(e) => {
+            e.preventDefault() ; 
+            studnetState.editMode ? dispatch({type : 'UpdateStudent'}) : dispatch({type : 'CreateStudent'})
+          }}>
+            <input type="text" value={studnetState.studentName} onChange={(el) => dispatch({type:'ChangeStudent_Name' , value : el.target.value}) } />
+            <button type="submit">{studnetState.editMode ? "Update Student" : "Add Student"}</button>
           </form>
   )
 }
