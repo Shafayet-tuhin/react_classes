26bba5b7fdb06ca290a81038f07a73cd605f8b2e
Shafayet-tuhin <shafayettuhin00@gmail.com>
2024-01-19 01:38:41 +0600

    API

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,59 @@
-
+import { useEffect, useState } from 'react'
 import './App.css'
-import StudentForm from './components/StudentForm';
-import AllStudentList from './components/AllStudentList';
-import Attendance from './components/Attendance';
 
-const App = () => {   
-  
+function App() {
+
+
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+
+  //  useEffect(() => {
+  //   fetch('https://jsonplaceholder.typicode.com/posts')
+  //     .then(response => response.json())
+  //     .then(response => {
+  //       setData(response)
+  //       console.log(response)
+  //     });
+  // },[]); // [] eta dite hobe must ... nahole loop e chole jabe 
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        setLoading(true);
+        return response.json();
+      })
+      .then(response => {
+        setLoading(false);
+        setData(response)
+        setError("")
+      })
+      .catch(err => {
+        setLoading(false);
+        setError(err.message)
+        setData([])
+      })
+  }, []);
 
   return (
-
     <div>
-          <StudentForm/>
 
-          <div className="student-section">
+       {loading && <h1>Loading.......</h1> }
+       {error && <h1>{error} </h1>}
 
-           <AllStudentList />
 
-            <Attendance />
-
-          </div>
+      <h2> API Clling  </h2>
+      <ul>
+        {
+          data.map(item => (
+            <li key={item.id}>
+              <h3>{item.id} :  {item.title}</h3>
+              <p>{item.body}</p>
+            </li>
+          ))
+        }
+      </ul>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import StudentProvider from './contexts/StudentProvider.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-
-   <StudentProvider>
-    <App /> 
-   </StudentProvider>
+   <React.StrictMode>
+   <App />
+ </React.StrictMode>,
 
 )
